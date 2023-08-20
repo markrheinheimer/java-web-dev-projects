f55b137a838992ea6a2dcbe2b7f437057cabe20e
[Mark Rheinheimer] <markrheinheimer@protonmail.comgit config --global user.name markrheinheimer>
2023-08-20 16:09:51 -0500

    second

--- datatypes/datatypes-exercises/src/main/java/org/launchcode/AliceWonderland.java
+++ datatypes/datatypes-exercises/src/main/java/org/launchcode/AliceWonderland.java
@@ -9,9 +9,22 @@ public class AliceWonderland {
                 " into the book her sister was reading, but it had no pictures or" +
                 " conversations in it, 'and what is the usr of a book,' thought Alice" +
                 " 'without pictures or conversation?'";
+        String lowerAlice = alice.toLowerCase();
         System.out.println("What word would you like to search for?");
         Scanner input = new Scanner(System.in);
         String response = input.next();
+        String lowerResponse = response.toLowerCase();
 
+        if (lowerAlice.contains(lowerResponse)) {
+            int index = lowerAlice.indexOf(lowerResponse);
+            System.out.println("We found the word at index " + index + ", and it has the length of " + lowerResponse.length());
+            String first = alice.substring(0, index - 1);
+            String second = alice.substring((index + response.length()));
+            System.out.println("Your new string with the word removed is: " + first + second);
+        }
+        else {
+            System.out.println("Word not found");
+        }
     }
 }
+
