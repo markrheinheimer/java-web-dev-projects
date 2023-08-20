5a3032471625ee7ea16172336ce1d7d25e37c002
rheins22 <127905208+rheins22@users.noreply.github.com>
2023-08-20 13:14:10 -0500

    Initial

---
+++ datatypes/src/main/java/org/launchcode/AliceWonderland.java
@@ -0,0 +1,28 @@
+package org.launchcode;
+import java.util.Scanner;
+
+public class AliceWonderland {
+    public static void main(String[] args) {
+        String alice = "Alice was beginning to get very" +
+                " tired of sitting by her sister on the" +
+                " bank, and of having nothing to do: once" +
+                " or twice she had peeped into the book" +
+                " her sister was reading, but it had no" +
+                " pictures or conversations in it, ‘and" +
+                " what is the use of a book,’ thought" +
+                " Alice ‘without pictures or conversation?";
+            System.out.println("What word would you like to search for?");
+            Scanner input = new Scanner(System.in);
+            String response = input.next();
+        if (alice.has(response)) {
+            System.out.println("We found it!!!");
+        } else if () {
+            
+        }
+
+    }
+
+
+
+    }
+}
